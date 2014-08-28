4c3a298c45613f8eaeba9692b134753856737941
Sarah de Voss <satv@itu.dk>
2014-08-28 13:00:54 +0200

    Hello again

--- GSStableMatching.java
+++ GSStableMatching.java
@@ -2,5 +2,6 @@ class GSStableMatching {
 	public static void main(String[] args){
 		System.out.println("Hello World");
                 System.out.println("more hello");
+                System.out.println("Hello Hello");
 	}
 }
