690c45a1380bef69d313a691480861637bc6ab0c
Mads Westi <madswesti@Hackintosh.local>
2014-09-03 22:53:47 +0200

    removed redundant stuff + print outs

--- GSStableMatching.java
+++ GSStableMatching.java
@@ -1,23 +1,18 @@
 import java.util.*;
 public class GSStableMatching {
-	private static boolean debug = false;
 	static public void main(String[] args) {
 		int N=0;
-		int personCount = 0;
 		ArrayList<Woman> women = new ArrayList<Woman>();
 		LinkedList<Man> men = new LinkedList<Man>();
-		
 		Scanner sc = new Scanner(System.in);
 			while (sc.hasNext()){
 				String token = sc.next();
-				if (token.equals("#") || token.startsWith("#")){
-					String line = sc.nextLine();
-					if (debug == true) System.out.println("Skipped: "+line);
+				if (token.startsWith("#")){
+					sc.nextLine();
 				}
 				else {
 					if (token.startsWith("n=")){
 						N = Integer.parseInt(token.substring(2));
-						if (debug == true) System.out.println("N=" + N);
 					}
 					else if(token.endsWith(":")) {
 						int id = Integer.parseInt(token.substring(0, token.length()-1));
@@ -29,20 +24,14 @@ public class GSStableMatching {
 						}
 					}
 					else{
-						if (personCount < 2*N){ 
-							int id = Integer.parseInt(token);
-
-							if (id%2 == 0) {
-								Woman w = new Woman((id-1)/2, sc.next(),N);
-								women.add(w);
-								if (debug == true) System.out.println(w.id +" "+ w.name);
-							}
-							else{
-								Man m = new Man((id-1)/2, sc.next());
-								men.add(m);
-								if (debug == true) System.out.println(m.id +" "+ m.name);
-							}
-							personCount++;
+						int id = Integer.parseInt(token);
+						if (id%2 == 0) {
+							Woman w = new Woman((id-1)/2, sc.next(), N);
+							women.add(w);
+						}
+						else{
+							Man m = new Man((id-1)/2, sc.next());
+							men.add(m);
 						}
 					}
 				}
@@ -61,9 +50,7 @@ public class GSStableMatching {
 		while(!men.isEmpty()){
 			Man currentMan = men.pop();
 			Woman currentWoman = women.get(currentMan.getNextPrority());
-			if(debug) System.out.println(currentMan.name+" proposes to "+currentWoman.name);
 			if (!currentWoman.isMarried()) {
-				if(debug) System.out.println("Gets married");
 				currentMan.marriedTo = currentWoman;
 				currentWoman.marriedTo = currentMan;
 				marriedMen[currentMan.id] = currentMan;
@@ -71,14 +58,12 @@ public class GSStableMatching {
 			else{
 				Man husband = (Man) currentWoman.marriedTo;
 				if(currentWoman.prioritiesById[currentMan.id] < currentWoman.prioritiesById[husband.id]){
-					if(debug) System.out.println(currentWoman.name + " switches partner to "+currentMan.name);
 					men.add(husband);
 					currentMan.marriedTo = currentWoman;
 					currentWoman.marriedTo = currentMan;
 					marriedMen[currentMan.id] = currentMan;
 				}
 				else{
-					if(debug) System.out.println("Gets rejected");
 					men.add(currentMan);
 				}
 			}
